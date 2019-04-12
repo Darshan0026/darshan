d61332d1cc03f728be23f5e0469adb53304c3a87
DARSHAN <darshan26rathod@gmail.com>
2019-04-12 12:30:11 +0530

    with shell

---
+++ repo_creator.sh
@@ -0,0 +1,14 @@
+mkdir darshan
+cd darshan
+git init
+echo "name:DARSHAN RATHOD, roll no:180020075 ">>"intro.txt"
+git add .
+git commit -m "my intro"
+git branch "interests"
+git checkout "interests"
+echo " interests: movies, pool  ">>"intro.txt"
+git add .
+git commit -m "added interest $(date +"%F %T" )"
+git checkout master
+git merge "interests"
+git remote add origin https://github.com/Darshan0026/darshan.git
